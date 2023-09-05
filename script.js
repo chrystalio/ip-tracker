57d7575dab1266e0d05ab7abda17bd2dac839aad
Chrystalio <chrystalio.k@outlook.com>
2023-09-05 21:00:44 +0700

    Add comment for better understanding script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Initialize leaflet map
 const map = L.map('map').setView([-6.315299, 106.699219], 6).addLayer(L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
