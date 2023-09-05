13ac0844cb9d98480ae76d52238989da25be5e79
Chrystalio <chrystalio.k@outlook.com>
2023-09-05 21:43:03 +0700

    Set DOM Element

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Set DOM element
+const [ipInput, form, ipAddress, locationIP, timeZone, ISP] = ["#search", "form", "ip-address", "location", "time-zone", "isp"].map(selector => document.querySelector(selector));
+
 // Initialize leaflet map
 const map = L.map('map').setView([-6.315299, 106.699219], 6).addLayer(L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
