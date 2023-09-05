fe9d3b6af7ba1f87643f9b91804d7cb24106f87a
Chrystalio <chrystalio.k@outlook.com>
2023-09-05 21:44:00 +0700

    Add function getIpInfo

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Set DOM element
 const [ipInput, form, ipAddress, locationIP, timeZone, ISP] = ["#search", "form", "ip-address", "location", "time-zone", "isp"].map(selector => document.querySelector(selector));
 
+// Fungtion to fetch data from API
+const getIpInfo = async (ip) => {
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
+    if(!response.ok) throw new Error('Failed to fetch iP Information');
+    return await response.json();
+}
+
 // Initialize leaflet map
 const map = L.map('map').setView([-6.315299, 106.699219], 6).addLayer(L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
